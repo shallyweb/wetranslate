66945b2bdd6f11fd7d6ff1cd4ecdc327d397adfe
melievsn <danielnwosu2009@gmail.com>
2025-12-13 23:24:08 +0100

    Add files via upload

---
+++ dictionary_assignments.py
@@ -0,0 +1,24 @@
+igbo_dict = {
+    'morning': 'ututu',
+    'fowl': 'okuko',
+    'head': 'isi',
+    'university': 'mahadum',
+    'book': 'akwukwo',
+    'phone': 'ekwenti',
+    'teacher': 'onye nkuzi',
+    'car': 'ugboala',
+    'God': 'chukwu',
+    'dog': 'nkita',
+    'fish': 'azu',
+    'hand': 'aka',
+    'leg': 'okpa',
+    'prayer': 'ekpele',
+    'food': 'nri',
+    'cloth': 'akwa',
+    'family': 'ezinulo',
+    'animal': 'anumanu',
+    'child': 'nwatakili',
+    'Thanks': 'daalu'
+}
+key = input('What English word do you want to translate to igbo? ')
+print(key + ' ' + ' ' 'is' + ' ' + ' ' + igbo_dict[key] + ' ' + ' ' + 'in  Igbo')
